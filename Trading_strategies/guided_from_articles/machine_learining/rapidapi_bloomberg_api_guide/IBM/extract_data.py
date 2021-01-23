656e3e064bac14c0cd44a897bd56d34a050ef436
michalkonik <michal.konikjob@gmail.com>
2021-01-23 19:21:43 +0100

    sklearn machine learning lib tested on first two tickers

---
+++ Trading_strategies/guided_from_articles/machine_learining/rapidapi_bloomberg_api_guide/IBM/extract_data.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+import numpy as np
+import datetime
+import json
+
+
+def extract_ticks(interval="y5"):
+    url = "https://bloomberg-market-and-financial-news.p.rapidapi.com/market/get-chart"
+
+    querystring = {"interval":interval,"id":"ibm%3Aus"}
+
+    headers = {
+        'x-rapidapi-host': "bloomberg-market-and-financial-news.p.rapidapi.com",
+        'x-rapidapi-key': "a9164563cbmshbb0d9669c26e1e6p1c432bjsn477e1068516e"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    json_d = response.json()
+    return json_d
+
+#Get year to date data ytd
+json_d = extract_ticks()
+print(json_d)
+
+# Write the data to json file
+with open("ibm_ytd.json","w") as fp:
+    json.dump(json_d,fp)
